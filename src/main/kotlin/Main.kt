30562e909733bd74a1e32938c86661e25adbf75c
LilianKanyua <liliankanyua@gmail.com>
2023-02-17 08:14:24 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(){
+     myString()
+    aboutMe("Alex",45)
+    strLength()
+    boolean("Frank")
+    boolean("Lilian")
+
+}
+fun myString(){
+    var myString= "AkiraChix"
+    var a=println(myString[1])
+    var b=println(myString[3])
+    var c=println(myString[4])
+    println("a " + "b " + "c")
+
+}
+fun aboutMe(name: String, age: Int) {
+    println("Hi, my name is $name and I am $age years old.")
+}
+fun strLength(){
+    var text="Adorable"
+    println(text.length)
+}
+fun boolean(name: String){
+     if (name=="Lilian")
+    println("That's me!")
+    else
+        println("I don't know who that is")
+    }
+
+
+
